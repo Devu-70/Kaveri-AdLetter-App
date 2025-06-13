9672124c172503f4063d9b0818a507bbb3728c34
Devu-70 <devudevzz001@gmail.com>
2025-06-13 15:23:36 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,35 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, getUserByKaveriId } = require('../controllers/userController');
+const { registerUser, getUserByKaveriId, getAllUsers, updateVerificationStatus } = require('../controllers/userController');
 const loginUser = require('../controllers/authController');
 const { verifyToken } = require('../middleware/tokenVerify');
 
 const {
     createLetter,
     getAllLetters,
-    getLettersByKaveriId
+    getLettersByKaveriId,
+    updateAuthorizationStatus
 } = require('../controllers/lettersGeneratedController');
 
 const {
     addTotalCredits,
     addUsedCredits,
-    getCreditLogs
+    getCreditLogs,
+    getAllCredits
 } = require('../controllers/creditUsageController');
 
 // User Routes
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
-router.get('/:kaveriId', verifyToken, getUserByKaveriId);
+router.get('/getall', getAllUsers);
+router.put('/verify/:kaveriId', updateVerificationStatus);
+
 
 // LettersGenerated Routes
 router.post('/letters', createLetter);
 router.get('/letters', getAllLetters);
 router.get('/letters/:kaveriId', getLettersByKaveriId);
+router.patch('/:id/authorize', updateAuthorizationStatus);
 
 // Credit Management Routes
 router.post('/credits/add-total', addTotalCredits); 
 router.post('/credits/add-used', addUsedCredits);  
+router.get('/credits', getAllCredits);
 router.get('/credit-logs/:kaveriId', getCreditLogs);
+
+
+router.get('/:kaveriId', verifyToken, getUserByKaveriId);
 
 module.exports = router;
